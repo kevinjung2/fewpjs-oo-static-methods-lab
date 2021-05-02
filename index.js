cf17159db3fccd4a6bc9bd83c4fd0a76103d5fa6
Kevin Jung <kev.jung.ii@gmail.com>
2021-05-02 12:36:58 -0400

    commit tree 55c3469ec8c769a33a7bc3033bd2b4630e35dc04

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    const helpers = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let split = this.capitalize(string).split(" ")
+    let titled = []
+    for (let word of split) {
+      if (!helpers.includes(word)) {
+        titled.push(this.capitalize(word))
+      } else {
+        titled.push(word)
+      }
+    }
+    return titled.join(" ")
+  }
 }
